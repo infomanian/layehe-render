4a85bd56c36ae9e0b416789b4ed26f6bd4143d10
Amir Rajabi <amirh_rajabi@yahoo.com>
2025-09-08 23:40:29 +0200

    model changed

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-if 1 == 1:
+if 1 == 10:
     ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL", "claude-opus-4-1-20250805") 
 else:
-    ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL", "claude-3-haiku-20240307")
+    ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL", "claude-sonnet-4-20250514")
 client = Anthropic(api_key=ANTHROPIC_API_KEY) if ANTHROPIC_API_KEY else None
 has_attachments = False
 
